03f55efa7b894c059f28d6c3059ef1700de4301b
Lart <tlartigau.pro@gmail.com>
2022-11-17 14:19:51 +0100

    env dependent firebaseConfig

--- src/firebaseconfig/index.ts
+++ src/firebaseconfig/index.ts
@@ -8,15 +8,26 @@ import { getFunctions } from "firebase/functions"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-export const firebaseConfig = {
-  apiKey: "AIzaSyC-doB1uIeNe8UsZiHri1jOrtjBeidPUJg",
-  authDomain: "staging-bachotage.firebaseapp.com",
-  projectId: "staging-bachotage",
-  storageBucket: "staging-bachotage.appspot.com",
-  messagingSenderId: "905539945097",
-  appId: "1:905539945097:web:caee44465bf5b46bf7fc58",
-  measurementId: "G-FVJNTLXFQT",
-}
+export const firebaseConfig =
+  process.env.NODE_ENV == "production"
+    ? {
+        apiKey: "AIzaSyAEONmJg_Kq-qBI0Z4wr9zbKY0eV9SU_ZE",
+        authDomain: "qoat-app.firebaseapp.com",
+        projectId: "qoat-app",
+        storageBucket: "qoat-app.appspot.com",
+        messagingSenderId: "52019349467",
+        appId: "1:52019349467:web:bd355f344391981bc6c9fe",
+        measurementId: "G-BJKE2GYM03",
+      }
+    : {
+        apiKey: "AIzaSyC-doB1uIeNe8UsZiHri1jOrtjBeidPUJg",
+        authDomain: "staging-bachotage.firebaseapp.com",
+        projectId: "staging-bachotage",
+        storageBucket: "staging-bachotage.appspot.com",
+        messagingSenderId: "905539945097",
+        appId: "1:905539945097:web:caee44465bf5b46bf7fc58",
+        measurementId: "G-FVJNTLXFQT",
+      }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
