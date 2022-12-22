a58297a26a9d264bcfebe10d38b90cdfdf716c7e
Lart <tlartigau.pro@gmail.com>
2022-12-22 20:43:24 +0100

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require("next-translate")
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
 const nextConfig = nextTranslate({
   reactStrictMode: true,
   swcMinify: false,
 })
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
